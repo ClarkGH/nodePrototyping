cf1f112aefe81e112dd898d4f86f254a294086bf
clarkgh <demolisho@hotmail.com>
2017-10-12 18:49:42 -0700

    basic setup

---
+++ basic-server/server.js
@@ -0,0 +1,18 @@
+// content of index.js
+const http = require('http')
+const port = 8080
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('The very basis of a node server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Error: ', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
