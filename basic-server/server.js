9cdea29bf4d792dca8807e853811ce548fa6595f
clarkgh <demolisho@hotmail.com>
2017-10-13 20:46:40 -0700

    monkey

--- basic-server/server.js
+++ basic-server/server.js
@@ -1,18 +1,18 @@
 // content of index.js
-const http = require('http')
-const port = 8080
+const http = require('http'),
+  port = 8080;
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('The very basis of a node server!')
+  console.log(request.url);
+  response.end('The very basis of a node server!');
 }
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('Error: ', err)
+    return console.log('Error: ', err);
   }
 
-  console.log(`server is listening on ${port}`)
-})
+  console.log(`server is listening on ${port}`);
+});
