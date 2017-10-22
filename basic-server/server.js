81ed1d3b778335c0800cc75d8bdeb5a921100a8b
clarkgh <demolisho@hotmail.com>
2017-10-21 18:56:48 -0700

    Playing with express/react/redux

--- basic-server/server.js
+++ basic-server/server.js
@@ -1,18 +1,10 @@
-// content of index.js
-const http = require('http'),
-  port = 8080;
+const express = require('express'),
+  app = express();
 
-const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end('The very basis of a node server!');
-}
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
 
-const server = http.createServer(requestHandler);
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('Error: ', err);
-  }
-
-  console.log(`server is listening on ${port}`);
-});
+app.listen(3000, () => {
+  console.log('App running on port 3000!');
+})
