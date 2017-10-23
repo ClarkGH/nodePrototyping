7c38255750817c94a08de03aa8279993519936fc
clarkgh <demolisho@hotmail.com>
2017-10-23 12:44:47 -0700

    New server setup for BE

---
+++ basic-server/server.js
@@ -0,0 +1,10 @@
+const express = require('express'),
+  path = require('path');
+
+app.get('/', (req, res) => {
+  res.send('Hello.');
+});
+
+app.listen(3000, () => {
+  console.log('Express server running on port 3000')
+})
